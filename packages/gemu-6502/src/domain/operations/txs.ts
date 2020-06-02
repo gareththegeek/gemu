e66df1314d3c9d5d51d8dc496a909aeeb6bdf22f
gareththegeek <gj_williams2000@yahoo.co.uk>
2020-06-02 15:56:17 +0100

    txs apparently does not modify flags

--- packages/gemu-6502/src/domain/operations/txs.ts
+++ packages/gemu-6502/src/domain/operations/txs.ts
@@ -1,15 +1,9 @@
 import { Event } from 'gemu-interfaces'
 import State from '../State'
 import { Bus } from 'gemu-interfaces'
-import isZero from '../bitwise/isZero'
-import isNegative from '../bitwise/isNegative'
 
 const txs = (state: State, _: Bus, __: number): Event<State> => ({
-    sp: state.x,
-    status: {
-        zero: isZero(state.x),
-        negative: isNegative(state.x)
-    }
+    sp: state.x
 })
 
 export default txs
